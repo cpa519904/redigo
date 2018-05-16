41f1eb413ffe8deb8cd76cfe8b7d5bce7d8d3e08
pingan.cui <pingan.cui@icaikee.com>
2018-05-16 15:28:49 +0800

    piping update

--- redis/redis.go
+++ redis/redis.go
@@ -35,6 +35,9 @@ type Conn interface {
 	// Do sends a command to the server and returns the received reply.
 	Do(commandName string, args ...interface{}) (reply interface{}, err error)
 
+	// DoNew sends a command to the server and returns the received reply.
+	DoNew(commandName string, args []interface{}) (reply interface{}, err error)
+
 	// Send writes the command to the client's output buffer.
 	Send(commandName string, args ...interface{}) error
 
